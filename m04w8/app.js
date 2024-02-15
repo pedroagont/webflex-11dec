3c3e6e1ed76d61c255cd983ab8878602df03243d
Pedro A. González <pgonzalez@post.com>
2024-02-15 15:08:10 -0500

    Add jquery notes and code

--- m04w8/app.js
+++ m04w8/app.js
@@ -1,59 +1,111 @@
-console.log('Hello from app.js!');
+$(document).ready(() => {
+  console.log('Hello from app.js!');
 
-// DOM
-console.log(navigator);
-console.log(window);
-console.log(document);
+  // DOM
+  // console.log(navigator);
+  // console.log(window);
+  // console.log(document);
 
-const notes = ['learn html', 'learn css', 'learn js'];
+  const notes = ['learn html', 'learn css', 'learn js'];
 
-// ---------------- HOW TO SELECT ELEMENTS
-console.log(document.getElementById('header-title'));
-const header = document.getElementById('header');
-const headerTitle = document.getElementById('header-title');
-const notesList = document.getElementById('notes-list');
-const newNoteForm = document.getElementById('new-note-form');
+  // ---------------- HOW TO SELECT ELEMENTS
+  // with vanilla js
+  // console.log(document.getElementById('header-title'));
+  // const header = document.getElementById('header');
+  // const headerTitle = document.getElementById('header-title');
+  // const notesList = document.getElementById('notes-list');
+  // const newNoteForm = document.getElementById('new-note-form');
 
-// ---------------- HOW TO CHANGE CONTENT
-headerTitle.innerHTML = 'Hello from app.js!';
+  // with jQuery
+  // console.log(jQuery);
+  // console.log($);
+  console.log($('#header-title'));
+  const header = $('#header');
+  const headerTitle = $('#header-title');
+  const notesList = $('#notes-list');
+  const newNoteForm = $('#new-note-form');
 
-// ---------------- HOW TO CHANGE STYLES
-header.style.backgroundColor = 'yellowgreen';
-header.style.color = 'black';
+  // // ---------------- HOW TO CHANGE CONTENT
+  // with vanilla js
+  // headerTitle.innerHTML = 'Hello from app.js!';
 
-// ---------------- HOW TO DYNAMIC RENDERING
-const renderNotes = () => {
-  notesList.innerHTML = '';
+  // with jQuery
+  headerTitle.html('Hello from app.js using jQuery!');
 
-  for (const note of notes) {
-    notesList.innerHTML += `
-        <article>
-          <h4>${note}</h4>
-        </article>`;
-  }
-};
+  // // ---------------- HOW TO CHANGE STYLES
+  // with vanilla js
+  // header.style.backgroundColor = 'yellowgreen';
+  // header.style.color = 'black';
 
-renderNotes();
+  // with jQuery
+  header.css('background-color', 'yellowgreen');
+  header.css('color', 'black');
 
-// ---------------- HOW TO ACCESS EVENTS
-const handleNewNoteFormSubmit = (event) => {
-  event.preventDefault();
-  console.log('Hello from submitting event!');
+  // // ---------------- HOW TO DYNAMIC RENDERING
+  const renderNotes = () => {
+    // with vanilla js
+    //   notesList.innerHTML = '';
+    //   for (const note of notes) {
+    //     notesList.innerHTML += `
+    //         <article>
+    //           <h4>${note}</h4>
+    //         </article>`;
+    //   }
 
-  //   console.log(event.target.elements.email.value);
-  const { content } = event.target.elements;
-  //   console.log(content.value);
+    // with jQuery
+    notesList.empty();
 
-  notes.push(content.value);
+    for (const note of notes) {
+      notesList.append(`
+            <article>
+                <h4>${note}</h4>
+            </article>`);
+    }
+  };
 
-  content.value = '';
   renderNotes();
-};
 
-newNoteForm.addEventListener('submit', handleNewNoteFormSubmit);
+  // // ---------------- HOW TO ACCESS EVENTS
+  // with vanilla js
+  // const handleNewNoteFormSubmit = (event) => {
+  //   event.preventDefault();
+  //   console.log('Hello from submitting event!');
 
-const handleWindowResize = () => {
-  console.log('resizing!');
-};
+  //   //   console.log(event.target.elements.email.value);
+  //   const { content } = event.target.elements;
+  //   //   console.log(content.value);
 
-window.addEventListener('resize', handleWindowResize);
+  //   notes.push(content.value);
+
+  //   content.value = '';
+  //   renderNotes();
+  // };
+
+  // newNoteForm.addEventListener('submit', handleNewNoteFormSubmit);
+
+  // const handleWindowResize = () => {
+  //   console.log('resizing!');
+  // };
+
+  // window.addEventListener('resize', handleWindowResize);
+
+  // with jQuery
+  const handleNewNoteFormSubmit = (event) => {
+    event.preventDefault();
+    console.log('Hello from submit jquery!');
+
+    const { content } = event.target.elements;
+
+    notes.push(content.value);
+
+    $(content).val('');
+    renderNotes();
+  };
+
+  // newNoteForm.on('submit', handleNewNoteFormSubmit);
+  newNoteForm.submit(handleNewNoteFormSubmit);
+
+  $(window).on('resize', () => {
+    console.log('resizing!');
+  });
+});
